19e76d6412c20a7c9436ffbec3806946ea889d4e
madjnr <danielmadubuihe@gmail.com>
2026-09-24 12:24:52 +0100

    feat(avatar): add variant prop to Avatar component for customizable styling

--- src/shared/components/avatar.tsx
+++ src/shared/components/avatar.tsx
@@ -5,19 +5,23 @@ interface AvatarProps {
   src?: ImageSourcePropType;
   fallback?: string;
   size?: "sm" | "md" | "lg";
+  variant?: "default" | "secondary";
 }
 export const Avatar = ({
   className,
   src,
   fallback,
   size = "md",
+  variant = "default",
 }: AvatarProps) => {
   return (
     <View
-      className={`bg-muted-foreground justify-center items-center rounded-full ${size === "sm" ? "w-10 h-10" : size === "md" ? "w-16 h-16" : "w-20 h-20"}`}
+      className={` justify-center items-center rounded-full ${size === "sm" ? "w-10 h-10" : size === "md" ? "w-16 h-16" : "w-20 h-20"}
+      ${variant === "secondary" ? "bg-primary-foreground/10 border border-border/50" : "bg-muted-foreground"}
+      `}
     >
       {src ? (
-        <Image source={src} className={className} />
+        <Image source={src} className={` ${className}`} />
       ) : (
         <Text className={className}>{fallback}</Text>
       )}
